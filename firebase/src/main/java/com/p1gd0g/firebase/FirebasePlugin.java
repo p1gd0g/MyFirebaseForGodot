2769b18c5a13ba06f74c476dd08719ca0ac27fbf
p1gd0g <renhao.szu@gmail.com>
2023-11-03 02:20:30 +0800

    Update Firebase dependencies and permissions

--- firebase/src/main/java/com/p1gd0g/firebase/FirebasePlugin.java
+++ firebase/src/main/java/com/p1gd0g/firebase/FirebasePlugin.java
@@ -2,11 +2,28 @@ package com.p1gd0g.firebase;
 
 import androidx.annotation.NonNull;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+import android.util.Log;
 import org.godotengine.godot.Godot;
 
+import java.util.Objects;
+
 public class FirebasePlugin extends org.godotengine.godot.plugin.GodotPlugin {
+
+    private FirebaseAnalytics mFirebaseAnalytics;
+    private static final String TAG = "FirebasePlugin";
+
+
     public FirebasePlugin(Godot godot) {
         super(godot);
+
+        try {
+            mFirebaseAnalytics = FirebaseAnalytics.getInstance(Objects.requireNonNull(getActivity()));
+            Log.d(TAG, "Firebase initialized.");
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+        }
+
     }
 
     @NonNull
